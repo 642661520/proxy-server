ae2ad04da48fa7856b914695fa541d0549510405
finalsummer <642661520@qq.com>
2026-07-03 21:53:01 +0800

    chore: remove client/ Vue SPA frontend
    The Web UI (Vue 3 + UnoCSS + 9 management pages) is no longer
    needed. The project now operates exclusively in YAML-driven
    proxy engine mode.

--- client/src/layouts/BlankLayout.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-</script>
-
-<template>
-  <div class="min-h-screen bg-slate-100 dark:bg-slate-900 flex items-center justify-center p-4">
-    <RouterView />
-  </div>
-</template>
